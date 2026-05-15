29eaff660acf2840fb10f020c7f1ddb0a7678075
Razonova Techno <soumalyachakraborty00@gmail.com>
2026-05-15 21:06:24 +0530

    lndc

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import authRoute from "../modules/auth/auth.routes";
 import userRoute from "../modules/user/user.routes";
 import rbacRoute from "../modules/rbac/rbac.routes";
+import branchRoute from "../modules/branch/branch.routes";
 
 const router = Router();
 
 router.use("/auth", authRoute);
 router.use("/users", userRoute);
 router.use("/rbac", rbacRoute);
+router.use("/branches", branchRoute);
 
 
 
