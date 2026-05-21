5dfccf6f69554548386e1f1d49eb9a2287b84bc5
Razonova Techno <soumalyachakraborty00@gmail.com>
2026-05-22 00:52:09 +0530

    fix:

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors({
   origin: [
-    process.env.FRONTEND_URL ||
+    process.env.FRONTEND_URL! ,
     "http://localhost:3000",
   ],
   credentials: true,
