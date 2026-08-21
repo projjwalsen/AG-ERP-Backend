e4c373c3fe3e21bc767eba7a9410a1ae5b690f8c
Razonova Techno <soumalyachakraborty00@gmail.com>
2026-08-21 21:54:52 +0530

    fix: Sale seed

--- prisma/migrations/20260821190000_link_hiring_journal_to_sale/migration.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "Journal" ADD COLUMN "saleId" TEXT;
-
-CREATE UNIQUE INDEX "Journal_saleId_key" ON "Journal"("saleId");
-
-ALTER TABLE "Journal" ADD CONSTRAINT "Journal_saleId_fkey"
-  FOREIGN KEY ("saleId") REFERENCES "Sale"("id") ON DELETE SET NULL ON UPDATE CASCADE;
